8d3c5912e5317335e889338d494eb03dc1ad0ffb
Hunter Sadler <hsadlersemail@gmail.com>
2014-11-19 02:48:16 -0600

    Add Cards API

---
+++ api/controllers/CardsController.js
@@ -0,0 +1,11 @@
+/**
+ * CardsController
+ *
+ * @description :: Server-side logic for managing cards
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	
+};
+
