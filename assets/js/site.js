cf56d84f2244fcd514f7976f06eeebaa14df432b
Hunter Sadler <hsadlersemail@gmail.com>
2014-12-11 20:17:12 -0600

    Add Animate When Quiz Is Loading, Check cache

--- assets/js/site.js
+++ assets/js/site.js
@@ -4,6 +4,17 @@ $(document).ready(function() {
 });
 
 $('#startQuizBtn').click(function() {
+  //Animate Fade
+  animating = true;
+  quizSetup = $('#quizSetup');
+  quizSetup.removeClass('fadeIn').addClass('fadeOut');
+  quizSetup.one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', setAnimateToFalse);
+  //Load Spinner
+  site = document.getElementById('site');
+  loadingSpinner = new Spinner().spin(site);
+
+
+  //Load Values
   var qClass = $('#classSelect').val();
   var qType = $('#typeSelect').val();
   var qSet = $('#setSelect').val();
@@ -26,13 +37,16 @@ $('#startQuizBtn').click(function() {
     //Load Card Name / Back / QuizSetup / Card Count
     mainCardName = $('#mainCardName');
     mainImage = $('#mainImage');
-    quizSetup = $('#quizSetup');
     cardsCount = $('#cardsCount');
     cardsTotal = $('#cardsTotal');
     console.log(quizDeck);
     //Hide Setup
-    quizSetup.removeClass('fadeIn').addClass('fadeOut');
-    quizSetup.one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', initialDataLoad);
+    if (animating == false) {
+      initialDataLoad();
+    }
+    else {
+        setTimeout(function() {initialDataLoad();}, 2000);
+    }
 
   });
 });
@@ -41,14 +55,15 @@ $('#mainImage').click(function() {
   if (animating == false) {
     if (mainImage.attr('side') == 'back') {
       animating = true;
-      mainImage.removeClass('fadeIn').addClass('fadeOut');
+      mainCardName.removeClass('initialAnimation');
+      mainImage.removeClass('initialAnimation').removeClass('fadeIn').addClass('fadeOut');
       mainImage.one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', loadNewCard);
     }
     else {
       animating = true;
       cardsCount.removeClass('fadeIn').addClass('fadeOut');
-      mainImage.removeClass('firstAnimation').removeClass('fadeIn').addClass('fadeOut');
-      mainCardName.removeClass('firstAnimation').removeClass('fadeIn').addClass('fadeOut');
+      mainImage.removeClass('fadeIn').addClass('fadeOut');
+      mainCardName.removeClass('fadeIn').addClass('fadeOut');
       mainImage.one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', loadCardBack);
     }
   }
@@ -56,9 +71,7 @@ $('#mainImage').click(function() {
 
 function initialDataLoad() {
   $('#quizSetup').hide();
-  //Display Load
-  site = document.getElementById('site');
-  loadingSpinner = new Spinner().spin(site);
+
   //Preload First 3 Images
   for (i = 0; i < 3; i++) {
     preloadImage(quizDeck[i].imageLink);
@@ -72,9 +85,14 @@ function initialDataLoad() {
   loadCardBack();
   //Stop Spinner
   loadingSpinner.stop();
+  //Make Sure First 3 Cards Are Cached
+  var cached = false;
+  while (cached == false) {
+    cached = setInterval(is_cached(quizDeck[2].imageLink), 200);
+  }
   //Display Card Quiz
-  mainCardName.addClass('firstAnimation');
-  mainImage.addClass('firstAnimation');
+  mainCardName.addClass('initialAnimation');
+  mainImage.addClass('initialAnimation');
   $('#cardQuiz').removeClass('hidden');
   $('#quizSetup').removeClass('fadeIn');
 }
