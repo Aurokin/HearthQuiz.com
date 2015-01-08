44a473da71387fdc02977a2022526308693e9718
Hunter Sadler <hsadlersemail@gmail.com>
2015-01-08 17:27:40 -0600

    Post Data On Lift

--- config/bootstrap.js
+++ config/bootstrap.js
@@ -10,7 +10,42 @@
  */
 
 module.exports.bootstrap = function(cb) {
+  //POST DATA
 
+  //Setup Data
+  var http = require('http');
+  var cards = require('../assets/json/AllSets.json');
+  var jsonData = JSON.stringify(cards);
+  var headers = {
+    'Content-Type': 'application/json',
+    'Content-Length': Buffer.byteLength(jsonData, 'utf8')
+  };
+  var options = {
+      host: 'localhost',
+      port: '1337',
+      path: '/insertCards',
+      method: 'POST',
+      headers: headers
+  };
+
+  //Setup Request
+  var reqs = http.request(options, function(res) {
+    res.setEncoding('utf-8');
+
+    var responseString = '';
+
+    res.on('data', function(data) {
+      console.log('POST completed');
+    });
+  });
+  reqs.on('error', function(e) {
+    // TODO: handle error.
+  });
+
+  //POST
+  reqs.write(jsonData);
+  reqs.end();
+  
   // It's very important to trigger this callback method when you are finished
   // with the bootstrap!  (otherwise your server will never lift, since it's waiting on the bootstrap)
   cb();
